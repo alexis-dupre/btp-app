df896431c600bf140dafee9e515fc193d022158a
Alexis Dupré <alexis@papymougeot.fr>
2026-09-13 14:27:13 +0200

    fix: remove unused Geist import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Manrope } from "next/font/google"
+import { Geist_Mono, Manrope } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
